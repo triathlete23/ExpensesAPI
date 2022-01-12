fa061cb5e0f41424b1aa2aa15eca2f23e9a8e300
Pavlo LIAHUSHYN <pavlo.liahushyn@axa.fr>
2022-01-12 15:08:44 +0100

    feat(db): add initial data to db + update version of EFCore packages

---
+++ ExpensesSummary.Repositories/Context/ExpenseDbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using ExpensesSummary.Repositories.Models;
+
+namespace ExpensesSummary.Repositories.Context
+{
+    public static class ExpenseDbInitializer
+    {
+        public static void Initialize(ExpenseContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            context.Users.Add(new User { Id = Guid.NewGuid(), Currency = "Dollar américain", Firstname = "Anthony", Lastname = "Stark" });
+            context.Users.Add(new User { Id = Guid.NewGuid(), Currency = "Rouble russe", Firstname = "Natasha", Lastname = "Romanova" });
+
+            context.SaveChanges();
+        }
+    }
+}
--- ExpensesSummary/Program.cs
+++ ExpensesSummary/Program.cs
@@ -1,5 +1,7 @@
+using ExpensesSummary.Repositories.Context;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,28 @@ namespace ExpensesSummary
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ExpenseContext>();
+                    ExpenseDbInitializer.Initialize(context);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
